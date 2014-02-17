2b71439183cd93f059c9dbaac93827dd094a9c98
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-02-17 14:03:27 +0100

    docs and small rearrange

--- reduction_scripts/do_all.py
+++ reduction_scripts/do_all.py
@@ -1,11 +1,8 @@
-# Define the paths for the various files
-# outpath must include at least one formatting string, "%s"
-# paths must include trailing slashes
-global_paths ={'AGBT12B_221_path':'/Users/adam/observations/gbt/AGBT12B_221_01/',
-               'AGBT14A_110_path':'/Users/adam/observations/gbt/AGBT14A_110_01/',
-               'outpath':'/Users/adam/observations/gbt/%smap/',
-              }
+"""
+Do all reductions in the appropriate order.
 
+Must have downloaded the data and set paths in paths.py first
+"""
 execfile('calibrate_C.py')
 execfile('calibrate_Ku.py')
 execfile('makecube_Lima_C.py')
--- reduction_scripts/paths.py
+++ reduction_scripts/paths.py
@@ -1,3 +1,7 @@
+# Define the paths for the various files
+# outpath must include at least one formatting string, "%s"
+# paths must include trailing slashes
+
 AGBT12B_221_path = '/Users/adam/observations/gbt/AGBT12B_221_01/'
 AGBT14A_110_path = '/Users/adam/observations/gbt/AGBT14A_110_01/'
 outpath = '/Users/adam/observations/gbt/%smap/'
