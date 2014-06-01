c4c5aa7c39625a3f8948d1bb1371ef7e905e47e4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-06-01 21:49:15 +0200

    swap the pointing information for the two feeds

--- reduction_scripts/makecube_Lima_Ka.py
+++ reduction_scripts/makecube_Lima_Ka.py
@@ -46,9 +46,20 @@ files = [os.path.join(outpath,x) for x in
           ]
          ]
 
-for fn in files:
-    makecube.add_file_to_cube(fn,
-                              cubename+'.fits',nhits=cubename+'_nhits.fits',
+file_pairs = zip(files[::2],files[1::2]) + zip(files[1::2],files[::2])
+
+for fn, fn2 in file_pairs:
+    data = pyfits.getdata(fn)
+    data2 = pyfits.getdata(fn2)
+    fileheader = pyfits.getheader(fn)
+    data['CRVAL2'] = data2['CRVAL2']
+    data['CRVAL3'] = data2['CRVAL3']
+
+    makecube.add_data_to_cube(data=data,
+                              fileheader=fileheader,
+                              filename=fn,
+                              cubefilename=cubename+'.fits',
+                              nhits=cubename+'_nhits.fits',
                               chmod=True,
                               add_with_kernel=True,
                               kernel_fwhm=10./3600.,
@@ -100,9 +111,19 @@ for cubename,restfreq,samplers in (
                                     (session,scan1,scan2,samplers[ii],sampler_feeds[samplers[ii]]))
                        for ii in xrange(len(samplers))]]
 
-    for fn in files:
-        makecube.add_file_to_cube(fn,
-                                  cubename+'.fits',
+    file_pairs = zip(files[::2],files[1::2]) + zip(files[1::2],files[::2])
+
+    for fn, fn2 in file_pairs:
+        data = pyfits.getdata(fn)
+        data2 = pyfits.getdata(fn2)
+        fileheader = pyfits.getheader(fn)
+        data['CRVAL2'] = data2['CRVAL2']
+        data['CRVAL3'] = data2['CRVAL3']
+
+        makecube.add_data_to_cube(data=data,
+                                  fileheader=fileheader,
+                                  filename=fn,
+                                  cubefilename=cubename+'.fits',
                                   nhits=cubename+'_nhits.fits',
                                   add_with_kernel=True,
                                   chmod=True,
@@ -114,47 +135,4 @@ for cubename,restfreq,samplers in (
     os.system('%s_starlink.sh' % cubename)
     makecube.make_flats(cubename,vrange=[-20,60],noisevrange=[150,200])
 
-
-#import FITS_tools
-from astropy.io import fits
-from FITS_tools.cube_regrid import gsmooth_cube,spatial_smooth_cube
-
-for cubename in ('LimaBean_H2CO33_cube', 'LimaBean_H213CO33_cube', 'LimaBean_H2C18O33_cube'):
-
-    cubename = os.path.join(outpath,cubename)
-    cube = fits.open(cubename+"_sub.fits")
-    # OUT OF DATE: for 2-2....
-    # kernel = ((2.5*60)**2 -  50.**2)**0.5 / sqrt(8*log(2)) = 60 arcsec
-    # 60 arcsec / 15 "/pixel = 4
-    cubesm2 = gsmooth_cube(cube[0].data, [5,4,4], use_fft=True, psf_pad=False, fft_pad=False)
-    cubesm = spatial_smooth_cube(cube[0].data, kernelwidth=4, interpolate_nan=True)
-    cube[0].data = cubesm
-    cube.writeto(cubename+"_sub_smoothtoCband.fits",clobber=True)
-    cube[0].data = cubesm2
-    cube.writeto(cubename+"_sub_smoothtoCband_vsmooth.fits",clobber=True)
-
-    # etamb = 56-64% * 1.37 from GTBpg
-    makecube.make_taucube(cubename,
-                          cubename+"_continuum.fits",
-                          etamb=1.37*0.6,
-                          tex=2.0,
-                          suffix="_sub_smoothtoCband_vsmooth.fits",
-                          outsuffix="_smoothtoCband_vsmooth.fits",
-                          TCMB=2.7315+0.4)
-
-    makecube.make_taucube(cubename,
-                          cubename+"_continuum.fits",
-                          etamb=1.37*0.6,
-                          tex=2.0,
-                          suffix="_sub_smoothtoCband.fits",
-                          outsuffix="_smoothtoCband.fits",
-                          TCMB=2.7315+0.4)
-
-    makecube.make_taucube(cubename,
-                          cubename+"_continuum.fits",
-                          etamb=1.37*0.6,
-                          tex=2.0,
-                          suffix="_sub.fits",
-                          outsuffix=".fits",
-                          TCMB=2.7315+0.4)
-
+execfile('make_taucubes_lima_ka.py')
